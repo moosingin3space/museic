8a92c9aa85798b55f600d138a064b94e38d170fb
nxfan <ninafan24@gmail.com>
2015-09-12 10:00:40 -0400

    sends battery info

--- muse.js
+++ muse.js
@@ -1,10 +1,15 @@
 var osc = require('node-osc');
+var ipc = require('ipc')
 
 function runServer() {
   var oscServer = new osc.Server(5000, '127.0.0.1');
   console.log('Listening.');
   oscServer.on("message", function (msg, rinfo) {
     console.log(msg);
+    if (msg[0]== '/muse/batt')
+    {
+      ipc.send('battery-status-update', msg[2]);
+    }
   });
 }
 
