b1a4ed1d0bd6a598e12443f561042bf62a8c3292
Nathan Moos <moosingin3space@gmail.com>
2015-09-12 17:45:28 -0400

    made api.js work with new API version

--- webapp/api.js
+++ webapp/api.js
@@ -1,12 +1,24 @@
 var $ = require('jquery');
 
-window.spotify_auth_token = 'please-change-me';
+window.spotify_access_token = 'please-change-me';
 
 var HOST = 'http://museic.azurewebsites.net';
 
 function fetch_next_songs(callback) {
-    if (window.spotify_auth_token != 'please-change-me') {
-        $.getJSON(HOST + '/song', callback);
+    if (window.spotify_access_token != 'please-change-me') {
+        $.ajax({
+            url: HOST + '/song',
+            headers: {
+                'X-Access-Token': window.spotify_access_token
+            },
+            success: function(data) {
+                if (data.status == 'success') {
+                    callback(data.body);
+                } else {
+                    console.error("XHR failed: " + data.body);
+                }
+            }
+        });
     } else {
         console.error("You need to set the variable `window.spotify_auth_token` to use this app");
     }
