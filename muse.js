e0d2c12b52bc03cf0939add0d84b45ca4625d67a
nxfan <ninafan24@gmail.com>
2015-09-12 10:36:43 -0400

    webContents

--- muse.js
+++ muse.js
@@ -1,14 +1,13 @@
 var osc = require('node-osc');
-var ipc = require('ipc')
 
-function runServer() {
+function runServer(webContents) {
   var oscServer = new osc.Server(5000, '127.0.0.1');
   console.log('Listening.');
   oscServer.on("message", function (msg, rinfo) {
     console.log(msg);
     if (msg[0]== '/muse/batt')
     {
-      ipc.send('battery-status-update', msg[2]);
+      webContents.send('battery-status-update', msg[2]);
     }
   });
 }
