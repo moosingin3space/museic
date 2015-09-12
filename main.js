bd4133cb6eb3b6c90eee57271b9f32fc445f1ef5
Nathan Moos <moosingin3space@gmail.com>
2015-09-12 01:00:40 -0400

    implemented basic electron app

---
+++ main.js
@@ -0,0 +1,24 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var muselib = require('./muse.js');
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/webapp/index.html');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
