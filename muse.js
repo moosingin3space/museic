27e46d2563461f04ed2bff53eae2230be90351d6
Nathan Moos <moosingin3space@gmail.com>
2015-09-12 22:44:04 -0400

    made it so muse.js only sends status every 15 sec

--- muse.js
+++ muse.js
@@ -2,81 +2,103 @@ var osc = require('node-osc');
 var EventEmitter = require('events');
 var EventCombiner = require('./event-combiner.js');
 var Neutral = {
-  Min: 1,
-  Max: 2
+    Min: 1,
+    Max: 2
 };
 
 function calcavg(msg)
 {
-  var sum = 0;
-  var avg = 0;
-  var amount = 0;
-  for (var i = 1; i < msg.length; i++)
-  {
-    if (msg[i] != 0)
+    var sum = 0;
+    var avg = 0;
+    var amount = 0;
+    for (var i = 1; i < msg.length; i++)
     {
-      sum += msg[i];
-      amount++;
+        if (msg[i] != 0)
+        {
+            sum += msg[i];
+            amount++;
+        }
     }
-  }
-  avg = sum / amount;
-  return avg;
+    avg = sum / amount;
+    return avg;
 }
 
 function runServer(webContents) {
-  var App = new EventEmitter();
-  var array = [0,0,0,0,0,0,0,0,0,0];
+    var App = new EventEmitter();
+    var array = [0,0,0,0,0,0,0,0,0,0];
+    var numNeutral = 0;
+    var numExcited = 0;
+    var numRelaxed = 0;
 
-  function record(alpha, beta)
-  {
-    beta = Math.pow(10, beta);
-    alpha = Math.pow(10, alpha);
-    var ratio = beta/ alpha;
-    array.shift();
-    array.push(ratio);
-    var arraysum = 0;
-    for(var i= 0; i < array.length; i++)
+    function record(alpha, beta)
     {
-      arraysum += array[i];
+        beta = Math.pow(10, beta);
+        alpha = Math.pow(10, alpha);
+        var ratio = beta/ alpha;
+        array.shift();
+        array.push(ratio);
+        var arraysum = 0;
+        for(var i= 0; i < array.length; i++)
+        {
+            arraysum += array[i];
+        }
+        console.log(arraysum/array.length);
+        var avg = arraysum/array.length;
+        if(avg > Neutral.Max)
+        {
+            numExcited++;
+        }
+        else if(avg < Neutral.Min)
+        {
+            numRelaxed++;
+        }
+        else
+        {
+            numNeutral++;
+        }
     }
-    console.log(arraysum/array.length);
-    var avg = arraysum/array.length;
-    if(avg > Neutral.Max)
-    {
-      webContents.send('mood-update', 'excited');
-    }
-    else if(avg < Neutral.Min)
-    {
-      webContents.send('mood-update', 'relaxed');
-    }
-    else {
-      webContents.send('mood-update', 'neutral');
-    }
-  }
 
-  EventCombiner.combineEvents(App, 'alpha_wave', 'beta_wave', record);
+    setTimeout(function sendMessage() {
+        if (numExcited > numRelaxed && numExcited > numNeutral) {
+            webContents.send('mood-update', 'excited');
+        }
+        if (numRelaxed > numExcited && numRelaxed > numNeutral) {
+            webContents.send('mood-update', 'relaxed');
+        }
+        if (numNeutral > numRelaxed && numNeutral > numExcited) {
+            webContents.send('mood-update', 'neutral');
+        }
 
-  var oscServer = new osc.Server(5000, '127.0.0.1');
-  console.log('Listening.');
-  oscServer.on("message", function (msg, rinfo) {
-    if (msg[0]== '/muse/batt')
-    {
-      console.log(msg);
-      webContents.send('battery-status-update', msg[1]/100);
-    }
-    if (msg[0] == '/muse/elements/beta_absolute')
-    {
-      var avg = calcavg(msg);
-      App.emit('beta_wave', avg);
-    }
-    else if (msg[0] == '/muse/elements/alpha_absolute')
-    {
-      var avg = calcavg(msg);
-      App.emit('alpha_wave', avg);
-    }
-  });
+        numNeutral = 0;
+        numRelaxed = 0;
+        numExcited = 0;
+
+        setTimeout(sendMessage, 15000);
+    }, 15000);
+
+    EventCombiner.combineEvents(App, 'alpha_wave', 'beta_wave', record);
+
+    var oscServer = new osc.Server(5000, '127.0.0.1');
+    console.log('Listening.');
+    oscServer.on("message", function (msg, rinfo) {
+        if (msg[0]== '/muse/batt')
+        {
+            console.log(msg);
+            webContents.send('battery-status-update', msg[1]/100);
+        }
+        if (msg[0] == '/muse/elements/beta_absolute')
+        {
+            var avg = calcavg(msg);
+            App.emit('beta_wave', avg);
+        }
+        else if (msg[0] == '/muse/elements/alpha_absolute')
+        {
+            var avg = calcavg(msg);
+            App.emit('alpha_wave', avg);
+        }
+    });
 }
 
 module.exports = {
-  'runServer': runServer
+    'runServer': runServer
 };
